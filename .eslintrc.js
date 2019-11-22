a635f0ddcea4605429dc3376b3ddf9c109927ec2
Andy Brown <asbrown002@gmail.com>
2019-11-21 16:15:38 -0800

    remove typechecking eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:@typescript-eslint/eslint-recommended',
     'prettier/@typescript-eslint',
   ],
   plugins: ['import'],
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-  },
   env: {
     es6: true,
     node: true,
