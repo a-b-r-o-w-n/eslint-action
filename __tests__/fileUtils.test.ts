52d80a966b120d1743d878d49c55c3866ff93222
Andy Brown <asbrown002@gmail.com>
2019-11-21 16:33:07 -0800

    filter deleted files

--- __tests__/fileUtils.test.ts
+++ __tests__/fileUtils.test.ts
@@ -1,10 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+
 import { filterFiles } from '../src/fileUtils';
+
+jest.mock('fs');
+jest.mock('path');
 
 describe('filterByGlob', () => {
   it('returns files that match any glob pattern', () => {
+    (path.resolve as jest.Mock).mockImplementation(path => path);
+    (fs.existsSync as jest.Mock).mockImplementation(() => true);
+
     const globs = ['src/**/*', 'test/**/*.js'];
     const files = ['src/foo/bar.ts', 'src/foo/baz.js', 'test/foo/bar.ts', 'test/foo/baz.js', 'config/env.json'];
 
     expect(filterFiles(files, globs)).toEqual(['src/foo/bar.ts', 'src/foo/baz.js', 'test/foo/baz.js']);
+  });
+
+  it('filters deleted files', () => {
+    (path.resolve as jest.Mock).mockImplementation(path => path);
+    (fs.existsSync as jest.Mock).mockImplementation(path => !path.includes('deleted-file'));
+
+    const globs = ['src/**/*', 'test/**/*.js'];
+    const files = ['src/foo/bar.ts', 'src/foo/baz.js', 'src/deleted-file.js'];
+
+    expect(filterFiles(files, globs)).toEqual(['src/foo/bar.ts', 'src/foo/baz.js']);
   });
 });
--- src/fileUtils.ts
+++ src/fileUtils.ts
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
 import micromatch from 'micromatch';
 
 export const filterFiles = (files: string[], globs: string[]): string[] => {
-  return micromatch(files, globs);
+  const result: string[] = [];
+  const filtered = micromatch(files, globs);
+
+  for (const file of filtered) {
+    if (fs.existsSync(path.resolve(file))) {
+      result.push(file);
+    }
+  }
+
+  return result;
 };
