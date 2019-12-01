ddde65722c2e9004ea6f8f657c820129a36d7877
James Ford <james.ford@bglgroup.co.uk>
2019-12-01 16:03:57 +0000

    Remove logs

--- src/eslint-action.ts
+++ src/eslint-action.ts
@@ -92,10 +92,8 @@ async function run(): Promise<void> {
   try {
     const oktokit = new github.GitHub(token);
     core.debug('Fetching files to lint.');
-    console.log(`filesGlob: ${filesGlob}, prNumber: ${prNumber}, getSha(): ${getSha()}`);
     const files = await getChangedFiles(oktokit, filesGlob, prNumber, getSha());
     core.debug(`${files.length} files match ${filesGlob}.`);
-    console.log(`${files.length} files match ${filesGlob}.`);
 
     if (files.length > 0) {
       const {
