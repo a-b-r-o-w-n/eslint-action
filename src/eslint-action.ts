369713ea1f6880854280576694641feffe39b2a4
Andy Brown <asbrown002@gmail.com>
2021-04-04 21:14:58 -0500

    output annotations using workflow commands

--- src/eslint-action.ts
+++ src/eslint-action.ts
@@ -6,7 +6,7 @@ import { getChangedFiles } from "./fileUtils";
 import { getPrNumber, getSha } from "./githubUtils";
 import inputs from "./inputs";
 import { lint } from "./lint";
-import { CHECK_NAME, processResults } from "./processResults";
+import { processResults } from "./processResults";
 
 const OWNER = github.context.repo.owner;
 const REPO = github.context.repo.repo;
@@ -30,53 +30,11 @@ async function run(): Promise<void> {
     core.debug(`${files.length} files match ${inputs.files}.`);
 
     if (files.length > 0) {
-      const {
-        data: { id: checkId },
-      } = await octokit.checks.create({
-        owner: OWNER,
-        repo: REPO,
-        started_at: new Date().toISOString(),
-        head_sha: getSha(),
-        status: "in_progress",
-        name: CHECK_NAME,
-      });
       const report = await lint(files);
-      const payload = processResults(report);
-      const maxChunk = 50;
+      const errorCount = processResults(report);
 
-      const annotationLength = payload?.output?.annotations?.length ?? 0;
-      if (payload?.output) {
-        if (annotationLength > maxChunk) {
-          const chunks = Math.ceil(annotationLength / maxChunk);
-          core.info(`There were ${annotationLength} annotations, splitting into ${chunks} requests`);
-          for (let index = 0; index < chunks; index++) {
-            const startIndex = index * maxChunk;
-            const endIndex = startIndex + maxChunk;
-            core.info(`Applying annotations ${startIndex} to ${startIndex + maxChunk}...`);
-            const returnValue = await octokit.checks.update({
-              owner: OWNER,
-              repo: REPO,
-              completed_at: new Date().toISOString(),
-              status: endIndex <= annotationLength ? "in_progress" : "completed",
-              check_run_id: checkId,
-              conclusion: payload.conclusion,
-              output: {
-                ...payload.output,
-                annotations: payload?.output?.annotations?.slice(startIndex, endIndex),
-              },
-            });
-            core.debug(`Got response with status of ${returnValue.status}, ${returnValue.data}`);
-          }
-        } else if (annotationLength <= maxChunk) {
-          await octokit.checks.update({
-            owner: OWNER,
-            repo: REPO,
-            completed_at: new Date().toISOString(),
-            status: "completed",
-            check_run_id: checkId,
-            ...payload,
-          });
-        }
+      if (errorCount > 0) {
+        core.setFailed(`${errorCount} error(s) found.`);
       }
     } else {
       core.info("No files to lint.");
--- src/processResults.ts
+++ src/processResults.ts
@@ -1,75 +1,62 @@
 import * as core from "@actions/core";
-import { RestEndpointMethodTypes } from "@octokit/rest";
-import { ESLint } from "eslint";
+import { ESLint, Linter } from "eslint";
 
 import inputs from "./inputs";
 
 const { GITHUB_WORKSPACE } = process.env;
 
-export const CHECK_NAME = "ESLint";
+function formatMessage(filePath: string, result: Linter.LintMessage) {
+  const { ruleId, line, column, message } = result;
 
-function logFileAnnotations(filePath: string, annotations: any[]) {
-  core.startGroup(filePath);
-  annotations.forEach((a) => {
-    const { message, annotation_level, start_line } = a;
+  let output = `file=${filePath}`;
 
-    if (annotation_level === "warning") {
-      core.warning(`${message} (Line ${start_line})`);
-    } else {
-      core.error(`${message} (Line ${start_line})`);
-    }
-  });
-  core.endGroup();
-}
-
-export function processResults(
-  results: ESLint.LintResult[]
-): Partial<RestEndpointMethodTypes["checks"]["update"]["parameters"]> {
-  const annotations: any[] = [];
-
-  let errorCount = 0;
-
-  for (const result of results) {
-    const { filePath, messages } = result;
-    const relFilePath = filePath.replace(`${GITHUB_WORKSPACE}/`, "");
-    const fileAnnotations: any[] = [];
-
-    for (const lintMessage of messages) {
-      const { line, severity, ruleId, message } = lintMessage;
-
-      // if ruleId is null, it's likely a parsing error, so let's skip it
-      if (!ruleId) {
-        continue;
-      }
-
-      if (severity === 2) {
-        errorCount++;
-      } else if (inputs.quiet) {
-        // skip message if quiet is true
-        continue;
-      }
-
-      fileAnnotations.push({
-        path: relFilePath,
-        start_line: line,
-        end_line: line,
-        annotation_level: severity === 2 ? "failure" : "warning",
-        message: `[${ruleId}] ${message}`,
-      });
-    }
-
-    annotations.push(...fileAnnotations);
-    if (core.isDebug()) {
-      logFileAnnotations(relFilePath, fileAnnotations);
-    }
+  if (line) {
+    output += `,line=${line}`;
   }
 
-  return {
-    conclusion: errorCount > 0 ? "failure" : "success",
-    output: {
-      title: CHECK_NAME,
-      summary: `${errorCount} error(s) found`,
-      annotations,
-    },
-  };
+  if (column) {
+    output += `,col=${column}`;
+  }
+
+  output += `::[${ruleId}] ${message}`;
+
+  return output;
+}
+
+export function processResults(lintResults: ESLint.LintResult[]): number {
+  let errorCount = 0;
+
+  for (const result of lintResults) {
+    const { filePath, messages } = result;
+    const relFilePath = filePath.replace(`${GITHUB_WORKSPACE}/`, "");
+
+    core.startGroup(relFilePath);
+
+    for (const message of messages) {
+      if (!message.ruleId) {
+        continue;
+      }
+
+      switch (message.severity) {
+        // warning
+        case 1:
+          if (inputs.quiet) break;
+
+          core.warning(formatMessage(relFilePath, message));
+          break;
+        // error
+        case 2:
+          errorCount++;
+
+          core.error(formatMessage(relFilePath, message));
+          break;
+        default:
+          break;
+      }
+    }
+
+    core.endGroup();
+  }
+
+  return errorCount;
 }
